8a3aae6d592931624b1ee09642a1a8b2d8c6c813
Santiago Piovesanel Lain <84496396+Psanti05@users.noreply.github.com>
2025-11-01 17:50:22 -0400

    Add files via upload

--- reversi.c
+++ reversi.c
@@ -1,11 +1,9 @@
-
 //
-// Author: 
-// Santiago Piovesanel Lain
-// Mar. 29, 2024
+// Author: Santiago Piovesanel Lain
 //
 
-#include "reversi.h"
+#include "lab8part2.h"
+#include "liblab8part2.h"
 
 #include <stdbool.h>
 #include <stdio.h>
@@ -57,7 +55,8 @@ bool checkLegalInDirection(char board[][26], int n, int row, int col,
   return false;
 }
 
-bool checkAllDirections(char board[][26], int n, int row, int col, char colour, bool direction[8]) {
+bool checkAllDirections(char board[][26], int n, int row, int col, char colour,
+                        bool direction[8]) {
   int directions = 0;
   for (int i = 0; i < 8; i++) {
     direction[i] = false;
@@ -101,12 +100,13 @@ bool checkAllDirections(char board[][26], int n, int row, int col, char colour, 
   }
 }
 
-void changeTiles(char board[][26], int n, int row, int col, char colour, bool direction[8]) {
+void changeTiles(char board[][26], int n, int row, int col, char colour,
+                 bool direction[8]) {
   board[row][col] = colour;
   int deltaRow = 0, deltaCol = 0;
   if (direction[0] == true) {
     deltaRow = -1, deltaCol = 0;
-    for (int i = 1; i < n; i++) { 
+    for (int i = 1; i < n; i++) {
       if (board[row + (i * deltaRow)][col + (i * deltaCol)] == colour) {
         break;
       } else {
@@ -116,7 +116,7 @@ void changeTiles(char board[][26], int n, int row, int col, char colour, bool di
   }
   if (direction[1] == true) {
     deltaRow = -1, deltaCol = 1;
-    for (int i = 1; i < n; i++) { 
+    for (int i = 1; i < n; i++) {
       if (board[row + (i * deltaRow)][col + (i * deltaCol)] == colour) {
         break;
       } else {
@@ -126,7 +126,7 @@ void changeTiles(char board[][26], int n, int row, int col, char colour, bool di
   }
   if (direction[2] == true) {
     deltaRow = 0, deltaCol = 1;
-    for (int i = 1; i < n; i++) { 
+    for (int i = 1; i < n; i++) {
       if (board[row + (i * deltaRow)][col + (i * deltaCol)] == colour) {
         break;
       } else {
@@ -136,7 +136,7 @@ void changeTiles(char board[][26], int n, int row, int col, char colour, bool di
   }
   if (direction[3] == true) {
     deltaRow = 1, deltaCol = 1;
-    for (int i = 1; i < n; i++) { 
+    for (int i = 1; i < n; i++) {
       if (board[row + (i * deltaRow)][col + (i * deltaCol)] == colour) {
         break;
       } else {
@@ -146,7 +146,7 @@ void changeTiles(char board[][26], int n, int row, int col, char colour, bool di
   }
   if (direction[4] == true) {
     deltaRow = 1, deltaCol = 0;
-    for (int i = 1; i < n; i++) { 
+    for (int i = 1; i < n; i++) {
       if (board[row + (i * deltaRow)][col + (i * deltaCol)] == colour) {
         break;
       } else {
@@ -156,7 +156,7 @@ void changeTiles(char board[][26], int n, int row, int col, char colour, bool di
   }
   if (direction[5] == true) {
     deltaRow = 1, deltaCol = -1;
-    for (int i = 1; i < n; i++) { 
+    for (int i = 1; i < n; i++) {
       if (board[row + (i * deltaRow)][col + (i * deltaCol)] == colour) {
         break;
       } else {
@@ -166,7 +166,7 @@ void changeTiles(char board[][26], int n, int row, int col, char colour, bool di
   }
   if (direction[6] == true) {
     deltaRow = 0, deltaCol = -1;
-    for (int i = 1; i < n; i++) { 
+    for (int i = 1; i < n; i++) {
       if (board[row + (i * deltaRow)][col + (i * deltaCol)] == colour) {
         break;
       } else {
@@ -176,7 +176,7 @@ void changeTiles(char board[][26], int n, int row, int col, char colour, bool di
   }
   if (direction[7] == true) {
     deltaRow = -1, deltaCol = -1;
-    for (int i = 1; i < n; i++) { 
+    for (int i = 1; i < n; i++) {
       if (board[row + (i * deltaRow)][col + (i * deltaCol)] == colour) {
         break;
       } else {
@@ -184,14 +184,131 @@ void changeTiles(char board[][26], int n, int row, int col, char colour, bool di
       }
     }
   }
-  
+}
+
+int calcBestMove(char board[][26], int n, char colour, bool direction[8], int score[][26]) {
+  int row, col;
+  int deltaRow = 0, deltaCol = 0;
+  int total = 0, number = 0, count = 0, newcount = 0;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (checkAllDirections(board, n, i, j, colour, direction)) {
+        row = i;
+        col = j;
+        checkAllDirections(board, n, row, col, colour, direction);
+        if (direction[0] == true) {
+          deltaRow = -1, deltaCol = 0;
+          for (int k = 1; k < n; k++) {
+            if (board[row + (k * deltaRow)][col + (k * deltaCol)] == colour) {
+              break;
+            } else {
+              score[row][col]++;
+            }
+          }
+        }
+        if (direction[1] == true) {
+          deltaRow = -1, deltaCol = 1;
+          for (int k = 1; k < n; k++) {
+            if (board[row + (k * deltaRow)][col + (k * deltaCol)] == colour) {
+              break;
+            } else {
+              score[row][col]++;
+            }
+          }
+        }
+        if (direction[2] == true) {
+          deltaRow = 0, deltaCol = 1;
+          for (int k = 1; k < n; k++) {
+            if (board[row + (k * deltaRow)][col + (k * deltaCol)] == colour) {
+              break;
+            } else {
+              score[row][col]++;
+            }
+          }
+        }
+        if (direction[3] == true) {
+          deltaRow = 1, deltaCol = 1;
+          for (int k = 1; k < n; k++) {
+            if (board[row + (k * deltaRow)][col + (k * deltaCol)] == colour) {
+              break;
+            } else {
+              score[row][col]++;
+            }
+          }
+        }
+        if (direction[4] == true) {
+          deltaRow = 1, deltaCol = 0;
+          for (int k = 1; k < n; k++) {
+            if (board[row + (k * deltaRow)][col + (k * deltaCol)] == colour) {
+              break;
+            } else {
+              score[row][col]++;
+            }
+          }
+        }
+        if (direction[5] == true) {
+          deltaRow = 1, deltaCol = -1;
+          for (int k = 1; k < n; k++) {
+            if (board[row + (k * deltaRow)][col + (k * deltaCol)] == colour) {
+              break;
+            } else {
+              score[row][col]++;
+            }
+          }
+        }
+        if (direction[6] == true) {
+          deltaRow = 0, deltaCol = -1;
+          for (int k = 1; k < n; k++) {
+            if (board[row + (k * deltaRow)][col + (k * deltaCol)] == colour) {
+              break;
+            } else {
+              score[row][col]++;
+            }
+          }
+        }
+        if (direction[7] == true) {
+          deltaRow = -1, deltaCol = -1;
+          for (int k = 1; k < n; k++) {
+            if (board[row + (k * deltaRow)][col + (k * deltaCol)] == colour) {
+              break;
+            } else {
+              score[row][col]++;
+            }
+          }
+        }
+      }
+    }
+  }
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (score[i][j] > 0) {
+        total ++;
+        newcount = score[i][j];
+        if (newcount > count) {
+          count = newcount;
+          number = total;
+        }
+      }
+    }
+  }
+  return number;
+}
+
+int makeMove(const char board[][26], int n, char turn, int *row, int *col) {
+    
 }
 
 int main(void) {
   int n = -1;
+  int score[26][26] = {0};
+  char colour = 'U';
   while (n < 0) {
     printf("Enter the board dimension: ");
     scanf("%d", &n);
+  }
+  printf("Computer plays (B/W): ");
+  while (!(colour == 'B' || colour == 'W')) {
+    scanf(" %c", &colour);
   }
 
   char board[26][26];
@@ -206,57 +323,170 @@ int main(void) {
   board[n / 2 - 1][n / 2] = 'B';
   printBoard(board, n);
 
-  char configs[3] = {'U', 'U', 'U'};
-  printf("Enter board configuration:\n");
-  while (configs[0] != '!' && configs[1] != '!' && configs[2] != '!') {
-    scanf("%*c", NULL);
-    scanf("%c%c%c", &configs[0], &configs[1], &configs[2]);
-
-    if (positionInBounds(n, configs[1] - 'a', configs[2] - 'a')) {
-      if (configs[0] == 'B') {
-        board[configs[1] - 'a'][configs[2] - 'a'] = 'B';
-      } else if (configs[0] == 'W') {
-        board[configs[1] - 'a'][configs[2] - 'a'] = 'W';
-      }
-    }
-  }
-  printBoard(board, n);
-
-  bool direction[8] = { 0 };
-  int possible = 0;
-  printf("Available moves for W:\n");
-  for (int i = 0; i < 26; i++) {
-    for (int j = 0; j < 26; j++) {
-      if (checkAllDirections(board, n, i, j, 'W', direction)) {
-        printf("%c%c\n", 'a' + i, 'a' + j);
-        possible++;
-      }
-    }
-  }
-  printf("Available moves for B:\n");
-  possible = 0;
-  for (int i = 0; i < 26; i++) {
-    for (int j = 0; j < 26; j++) {
-      if (checkAllDirections(board, n, i, j, 'B', direction)) {
-        printf("%c%c\n", 'a' + i, 'a' + j);
-        possible++;
-      }
-    }
-  }
-
-  char move[3] = {'U', 'U', 'U'};
-  printf("Enter a move:\n");
-  scanf("%*c", NULL);
-  scanf("%c%c%c", &move[0], &move[1], &move[2]);
-
-  if (checkAllDirections(board, n, move[1] - 'a', move[2] - 'a', move[0], direction)) {
-    printf("Valid move.\n");
-    checkAllDirections(board, n, move[1] - 'a', move[2] - 'a', move[0], direction);
-    changeTiles(board, n, move[1] - 'a', move[2] - 'a', move[0], direction);
+  bool computer = true;  // bool that says if it's computer's turn
+  if (colour == 'B') {   // if computer is B, it gets first turn
+    computer = true;
   } else {
-    printf("Invalid move.\n");
+    computer = false;
+  }
+  bool running = true;
+  int row, col;
+  char rowC, colC;
+  bool direction[8] = {0};
+  int possibleW = 0, possibleB = 0;
+  int counter = 0;
+  bool playerLoss = false;
+  bool action = true;
+
+  while (running == true) {
+    if (action == true) {
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+          score[i][j] = 0;
+        }
+      }
+      counter = 0;
+      possibleW = 0;
+      possibleB = 0;
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+          if (checkAllDirections(board, n, i, j, 'W', direction)) {
+            possibleW++;
+          }
+        }
+      }
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+          if (checkAllDirections(board, n, i, j, 'B', direction)) {
+            possibleB++;
+          }
+        }
+      }
+    }
+
+    if (possibleW == 0 && possibleB == 0) {
+      running = false;
+    } else if (colour == 'B') {  // if computer has black
+      if (computer == true) {    // if it is computer's turn
+        if (possibleB > 0) {
+          counter = calcBestMove(board, n, 'B', direction, score);
+          for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+              if (checkAllDirections(board, n, i, j, 'B', direction)) {
+                counter --;
+                if (counter == 0) {
+                  row = i;
+                  col = j;
+                }
+              }
+            }
+          }
+          checkAllDirections(board, n, row, col, 'B', direction);
+          changeTiles(board, n, row, col, 'B', direction);
+          printf("Computer places B at %c%c.\n", row + 'a', col + 'a');
+          printBoard(board, n);
+          computer = false;
+          action = true;
+        } else {
+          printf("B player has no valid move.\n");
+          computer = false;
+          action = false;
+        }
+      } else {  // if it is player's turn
+        if (possibleW > 0) {
+          printf("Enter move for colour W (RowCol): ");
+          scanf(" %c%c", &rowC, &colC);
+          if (checkAllDirections(board, n, rowC - 'a', colC - 'a', 'W', direction)) {
+            checkAllDirections(board, n, rowC - 'a', colC - 'a', 'W', direction);
+            changeTiles(board, n, rowC - 'a', colC - 'a', 'W', direction);
+            printBoard(board, n);
+          } else {
+            printf("Invalid move.\n");
+            playerLoss = true;
+            running = false;
+          }
+          computer = true;
+          action = true;
+        } else {
+          printf("W player has no valid move.\n");
+          computer = true;
+          action = false;
+        }
+      }
+    } else {  // if computer has white
+      if (computer == true) {    // if it is computer's turn
+        if (possibleW > 0) {
+          counter = calcBestMove(board, n, 'W', direction, score);
+          for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+              if (checkAllDirections(board, n, i, j, 'W', direction)) {
+                counter --;
+                if (counter == 0) {
+                  row = i;
+                  col = j;
+                }
+              }
+            }
+          }
+          checkAllDirections(board, n, row, col, 'W', direction);
+          changeTiles(board, n, row, col, 'W', direction);
+          printf("Computer places W at %c%c.\n", row + 'a', col + 'a');
+          printBoard(board, n);
+          computer = false;
+          action = true;
+        } else {
+          printf("W player has no valid move.\n");
+          computer = false;
+          action = false;
+        }
+      } else {  // if it is player's turn
+        if (possibleB > 0) {
+          printf("Enter move for colour B (RowCol): ");
+          scanf(" %c%c", &rowC, &colC);
+          if (checkAllDirections(board, n, rowC - 'a', colC - 'a', 'B', direction)) {
+            checkAllDirections(board, n, rowC - 'a', colC - 'a', 'B', direction);
+            changeTiles(board, n, rowC - 'a', colC - 'a', 'B', direction);
+            printBoard(board, n);
+          } else {
+            printf("Invalid move.\n");
+            playerLoss = true;
+            running = false;
+          }
+          computer = true;
+          action = true;
+        } else {
+          printf("B player has no valid move.\n");
+          computer = true;
+          action = false;
+        }
+      }
+    }
   }
   
-  printBoard(board, n);
+  if (playerLoss == true) {
+    printf("%c player wins.\n", colour);
+  } else {
+    int countB = 0;
+    int countW = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (board[i][j] == 'B') {
+          countB++;
+        }
+        if (board[i][j] == 'W') {
+          countW++;
+        }
+      }
+    }
+    if (countB > countW) {
+      printf("B player wins.\n");
+    } else if (countW > countB) {
+      printf("W player wins.\n");
+    } else {
+      printf("Draw!\n");
+    }
+  }
+  
   return 0;
 }
+
